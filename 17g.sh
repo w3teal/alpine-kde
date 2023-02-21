7d09019738a1408bb8ed9fddf27689bcfc0806fb
sulincix <parduscix@yandex.ru>
2023-02-21 15:23:10 +0000

    Update 17g.sh

--- 17g.sh
+++ 17g.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
-apk add py3-yaml py3-gobject3 py3-pyparted tzdata util-linux coreutils rsync gparted setxkbmap
-apk add make gettext wget
-wget https://gitlab.com/ggggggggggggggggg/17g/-/archive/master/17g-master.zip -O /tmp/17g.zip
 cd /tmp
-unzip 17g.zip
-cd  /tmp/17g-master/
-make && make install
+wget https://github.com/alpine-kde/17g-alpine/releases/download/current/17g-installer-4.4.1-r0.apk -O 17g.apk
+apk add 17g.apk --allow-untrusted
 ln -s true /bin/locale-gen
