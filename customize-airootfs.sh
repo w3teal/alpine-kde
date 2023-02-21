5a224e63c76835974580c3de1f08916ffa6398af
sulincix <parduscix@yandex.ru>
2023-02-21 21:09:14 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -2,12 +2,19 @@
 apk update
 apk upgrade
 setup-xorg-base || true
-apk add nano chromium plasma-pa plasma-desktop plasma-nm dolphin konsole flatpak ca-certificates sddm
+# kde
+apk add plasma-pa plasma-desktop plasma-nm sddm systemsettings
+apk add ark dolphin konsole kate gwenview okular spectacle
+# apps
+apk add nano chromium ca-certificates flatpak 
+# flatpak
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-apk add elogind polkit-elogind dbus
-apk add wpa_supplicant networkmanager pulseaudio pulseaudio-alsa
+# logind and other stuff
+apk add elogind polkit-elogind dbus wpa_supplicant networkmanager pulseaudio \
+    pulseaudio-alsa bluez 
 echo -e "live\nlive\n" | passwd root
 rc-update add dbus
+rc-update add bluetooth
 rc-update add networkmanager
 rc-update add wpa_supplicant
 rc-update add sddm
