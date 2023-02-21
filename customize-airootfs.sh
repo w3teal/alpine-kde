eb49f068bafdb8b551e447130b29d8b7669fc204
sulincix <parduscix@yandex.ru>
2023-02-21 15:32:09 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -2,8 +2,8 @@
 apk update
 apk upgrade
 setup-xorg-base || true
-apk add nano chromium-browser
-apk add plasma elogind polkit-elogind dbus
+apk add nano chromium plasma
+apk add elogind polkit-elogind dbus
 apk add wpa_supplicant networkmanager pulseaudio pulseaudio-alsa
 echo -e "live\nlive\n" | passwd root
 rc-update add dbus
