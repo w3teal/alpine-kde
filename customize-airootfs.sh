6ee6272a62e9e87680f392a41aaffcfa97442f7e
sulincix <parduscix@yandex.ru>
2023-02-21 15:38:38 +0000

    Update customize-airootfs.sh

--- customize-airootfs.sh
+++ customize-airootfs.sh
@@ -2,13 +2,14 @@
 apk update
 apk upgrade
 setup-xorg-base || true
-apk add nano chromium plasma
+apk add nano chromium plasma flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 apk add elogind polkit-elogind dbus
 apk add wpa_supplicant networkmanager pulseaudio pulseaudio-alsa
 echo -e "live\nlive\n" | passwd root
 rc-update add dbus
 rc-update add networkmanager
 rc-update add wpa_supplicant
-rc-update add lightdm
+rc-update add sddm
 rc-update add udev
 rc-update add elogind
