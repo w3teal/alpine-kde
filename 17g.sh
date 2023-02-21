6fb64099c5228225a05f0d890d691faea1f40949
sulincix <parduscix@yandex.ru>
2023-02-21 15:45:04 +0000

    Update 17g.sh

--- 17g.sh
+++ 17g.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/bin/bash
 cd /tmp
-wget https://github.com/alpine-kde/17g-alpine/releases/download/current/17g-installer-4.4.1-r0.apk -O 17g.apk
-apk add 17g.apk --allow-untrusted
-ln -s true /bin/locale-gen
+wget https://github.com/alpine-kde/17g-alpine/releases/download/current/17g-installer-4.4.1-r0.apk
+apk add *.apk --allow-untrusted
+ln -s true /bin/locale-gen || true
